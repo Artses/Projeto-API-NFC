2721a93919785c9c1d63f46027508db99fcf4e38
Arthur Eustaquio <arthurreustaquio@gmail.com>
2024-10-14 21:06:46 -0300

    tratamento de exceção corrigido

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -9,19 +9,9 @@ import { ApiTags } from '@nestjs/swagger';
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
-  @Post()
-  create(@Body() createUsuarioDto: CreateUsuarioDto) {
-    return this.usuarioService.create(createUsuarioDto);
-  }
-
   @Get()
   findAll() {
     return this.usuarioService.findAll();
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
-    return this.usuarioService.update(+id, updateUsuarioDto);
   }
 
   @Delete(':id')
