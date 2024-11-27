22c85554e929e7d98af961460e71deb2dbaf33db
Arthur Eustaquio <arthurreustaquio@gmail.com>
2024-11-27 16:35:06 -0300

    materia removed

--- src/materia/dto/update-materia.dto.ts
+++
@@ -1,8 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMateriaDto } from './create-materia.dto';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class UpdateMateriaDto extends PartialType(CreateMateriaDto) {
-    @ApiProperty({example: 1})
-    nome: string
-}
