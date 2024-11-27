59a202e3912e231111e32bf463d5e082d4c1c86e
Arthur Eustaquio <arthurreustaquio@gmail.com>
2024-11-27 16:31:16 -0300

    Commit final

--- src/aluno/dto/create-aluno.dto.ts
+++ src/aluno/dto/create-aluno.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateAlunoDto {
+    @ApiProperty({example: "12302546"})
+    id: number; 
     @ApiProperty({example: "Aluno"})
     nome: string; 
 }
